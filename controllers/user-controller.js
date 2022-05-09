9d879bbfb4ea5517ff988a20752ab2774b21cb2e
hitpop2216 <hitpop2216@gmail.com>
2022-05-10 01:09:32 +0800

    Complete register & login route

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,10 +1,21 @@
 const { User } = require('../models')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
 const userController = {
   register: async (req, res, next) => {
     try {
-      const user = await User.create(req.body)
+      if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords are not match!')
+
+      if (await User.findOne({ where: { account: req.body.account } })) throw new Error('Account is already registered!')
+
+      const user = await User.create({
+        account: req.body.account,
+        name: req.body.name,
+        email: req.body.email,
+        password: await bcrypt.hash(req.body.password, 10),
+        role: req.body.role === 'admin' ? 'admin' : 'user'
+      })
       res.json({ status: 'success', user })
     } catch (err) {
       next(err)
