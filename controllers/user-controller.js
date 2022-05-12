0cc2f9f6e2d614552ba8723400d83f327d79217b
hitpop2216 <hitpop2216@gmail.com>
2022-05-12 08:10:59 +0800

    getUser

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -39,71 +39,8 @@ const userController = {
           { model: User, as: 'Followings' }
         ]
       })
-      res.json({ status: 'success', data:{ user }})
-    } catch (err) {
-      next(err)
-    }
-  },
-  getTweets: async (req, res, next) => {
-    try {
-      const userTweets = await Tweet.findAll({
-        where: { userId: req.params.id },
-        order: [['createdAt', 'DESC']]
-      })
-      res.json({ status: 'success', data:{ userTweets }})
-    } catch (err) {
-      next(err)
-    }
-  },
-  getRepliedTweets: async (req, res, next) => {
-    try {
-      const repliedTweets = await Reply.findAll({
-        where: { userId: req.params.id },
-        order: [['createdAt', 'DESC']]
-      })
-      res.json({ status: 'success', data:{ repliedTweets }})
-    } catch (err) {
-      next(err)
-    }
-  },
-  getLikes: async (req, res, next) => {
-    try {
-      const likes = await Like.findAll({
-        where: { userId: req.params.id },
-        order: [['createdAt', 'DESC']]
-      })
-      res.json({ status: 'success', data:{ likes }})
-    } catch (err) {
-      next(err)
-    }
-  },
-  getFollowings: async (req, res, next) => {
-    try {
-      const followings = await User.findByPk(req.params.id, {
-        include: [{ model: User, as: 'Followings' }]
-      })
-      res.json({ status: 'success', data:{ followings: followings.Followings }})
-    } catch (err) {
-      next(err)
-    }
-  },
-  getFollowers: async (req, res, next) => {
-    try {
-      const followers = await User.findByPk(req.params.id, {
-        include: [{ model: User, as: 'Followers' }]
-      })
-      res.json({ status: 'success', data: { followers: followers.Followers } })
-    } catch (err) {
-      next(err)
-    }
-  },
-  putUser: async (req, res, next) => {
-    try {
-      const { password, checkPassword } = req.body
-      if (password !== checkPassword) throw new Error('密碼與確認密碼不符。')
-      const user = await User.findByPk(getUser(req).id)
-      await user.update(req.body)
-      res.json({ status: 'success', message: '資料更改成功！', data: { user  }})
+      req.user.dataValues.clickUser = user
+      res.status(200).json(req.user.dataValues)
     } catch (err) {
       next(err)
     }
